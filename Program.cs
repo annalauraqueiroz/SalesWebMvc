2b8a8d9569382651ef4943aa27740cfd02010e1c
Anna <annalauraqueiroz@hotmail.com>
2023-03-28 19:35:30 -0300

    Delete seller

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddDbContext<SalesWebMvcContext>(options =>
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<DbInitializer>();
 builder.Services.AddScoped<SellerService>();
+builder.Services.AddScoped<DepartmentService>();
 
 var app = builder.Build();
 
